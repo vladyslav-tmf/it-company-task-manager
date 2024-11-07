512f8b358d70cf113de23a7ac8875f1435cf7982
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-07 16:44:04 +0200

    add TaskType, Position, Worker and Task models.
    Added new models for managing tasks and workers: TaskType for task categories, Position for job positions, Worker as an extension of AbstractUser for workers with positions, and Task to store task details such as priority, deadline, and assigned workers.
    
    Also, added __str__ method to each model for better representation

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class NamedModel(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskType(NamedModel):
+    class Meta:
+        verbose_name = "task type"
+        verbose_name_plural = "task types"
+
+
+class Position(NamedModel):
+    pass
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        to=Position, on_delete=models.CASCADE, related_name="workers"
+    )
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = "Low", "Low priority"
+        STANDARD = "Standard", "Standard priority"
+        HIGH = "High", "High priority"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=Priority.choices)
+    task_type = models.ForeignKey(
+        to=TaskType, on_delete=models.CASCADE, related_name="tasks"
+    )
+    assignees = models.ManyToManyField(to=Worker, related_name="tasks")
+
+    class Meta:
+        ordering = ["deadline", "-priority"]
+
+    def __str__(self) -> str:
+        return (f"{self.name} (Deadline: {self.deadline}, "
+                f"Priority: {self.get_priority_display()})")
