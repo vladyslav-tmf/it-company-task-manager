98d4142bb5bea3de2014edb6e1f4c8cf098ff635
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-30 08:55:55 +0200

    Add email configuration, dotenv support and .env.sample for email configuration
    - Enabled loading environment variables using python-dotenv to secure sensitive information such as email credentials.
    - Email settings for SMTP with Gmail are configured, and a password reset timeout is set.
    - Added a .env.sample file to guide users in setting up email configurations.

--- core/settings.py
+++ core/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +132,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_PORT = 587
+
+PASSWORD_RESET_TIMEOUT = 14400
