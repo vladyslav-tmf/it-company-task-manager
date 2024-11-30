c8e9e76c797854f697ca41e9e77bd04ba42c928c
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-30 13:24:44 +0200

    Add static assets and add scroll and count up scripts
    - Added static assets including images and CSS files for the project's design system. It also adds JavaScript functionality to maintain scroll position across pagination and implements a count-up animation for various elements.

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    # third-party apps:
+    "widget_tweaks",
+
     #user apps:
     "accounts",
     "tasks"
@@ -129,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
