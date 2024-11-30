2120a4c02d27024e755f1fff7a4ef1fd5e81276d
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-30 08:14:17 +0200

    Add custom user model "Worker" extending AbstractUser
    - Introduced a Worker model with position field, extending Django's AbstractUser with unique email addresses.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Worker(AbstractUser):
+    email = models.EmailField(unique=True)
+    position = models.ForeignKey("Position", on_delete=models.CASCADE, related_name="workers", null=True, blank=True)
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
