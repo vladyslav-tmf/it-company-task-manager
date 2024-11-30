02d1c0c02e94a6f3a8b66d058ed60f830cf523c4
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-30 08:09:16 +0200

    Update installed apps and change time zone to Europe/Kiev
    - Added "accounts" and "tasks" to the list of installed apps.
    - Changed the time zone setting from "UTC" to "Europe/Kiev".

--- core/settings.py
+++ core/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # build-in apps:
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    #user apps:
+    "accounts",
+    "tasks"
 ]
 
 MIDDLEWARE = [
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
